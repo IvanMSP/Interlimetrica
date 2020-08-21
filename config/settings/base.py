ef18d19ab5d04be463cbc6063a2ad77850e6fcad
IvanMSP <ivanlopz11@gmail.com>
2020-08-21 03:00:55 -0500

    FIX-Settings config

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).ancestor(3)
 sys.path.append(BASE_DIR.child('apps'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = config('SECRET_KEY')
 
 
 # Application definition
