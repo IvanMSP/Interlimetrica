1bb8e28ae2c25efec9c21ab7592d44716c4ebc4e
IvanMSP <ivanlopz11@gmail.com>
2020-08-21 01:51:10 -0500

    DEPLOY-Heroku

---
+++ config/settings/heroku.py
@@ -0,0 +1,24 @@
+from .base import *
+import dj_database_url
+from decouple import config
+
+DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+SECRET_KEY = os.environ['SECRET_KEY']
